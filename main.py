d84dd255b1100be18fd1c6d9f20962aba81b81a2
sshikha78 <sharma07@gwu.edu>
2023-04-11 00:39:41 -0400

    EDA

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ print(df.shape)
 
 # Check for missing values in the DataFrame
 print(df.isna().sum())
-#Found 201 NULL values in bmi column
+# Found 201 NULL values in bmi column
 
 # Get summary statistics for the numerical columns in the DataFrame
 print(df.describe())
@@ -31,19 +31,11 @@ for col in df.columns:
   if df[col].dtype != 'float64':
     print(f"{col} has unique values:{df[col].unique()}")
 
-#Filling Null Data
-#We will fill the null values of bmi column with the mean of this column
+
+# Filling Null Data
+# We will fill the null values of bmi column with the mean of this column
 df['bmi'].fillna(value=df['bmi'].mean(),inplace=True)
 print("after filling null values:",df.isna().sum())
-
-
-# Convert categorical variables into numerical using one-hot encoding
-df = pd.get_dummies(df, columns=['gender', 'ever_married', 'work_type', 'Residence_type', 'smoking_status'])
-
-# Normalize the numerical variables
-df['age'] = (df['age'] - df['age'].min()) / (df['age'].max() - df['age'].min())
-df['avg_glucose_level'] = (df['avg_glucose_level'] - df['avg_glucose_level'].min()) / (df['avg_glucose_level'].max() - df['avg_glucose_level'].min())
-df['bmi'] = (df['bmi'] - df['bmi'].min()) / (df['bmi'].max() - df['bmi'].min())
 
 # Drop the ID column as it is not relevant for modeling
 df.drop('id', axis=1, inplace=True)
@@ -51,16 +43,10 @@ df.drop('id', axis=1, inplace=True)
 # Convert the stroke column to integers (0 for no stroke, 1 for stroke)
 df['stroke'] = df['stroke'].astype(int)
 
-#PRE PROCESSING + EDA
+# PRE PROCESSING + EDA
 
-# Plot a histogram of the age variable
-plt.hist(df['age'], bins=20)
-plt.title('Age Distribution')
-plt.xlabel('Age')
-plt.ylabel('Frequency')
-plt.show()
-
-#Residence Type, Average Glucose Level, BMI, and Smoking Status
+# Target feature - Stroke
+print("Value count in the stroke : \n",df['stroke'].value_counts())
 
 # Plot a bar chart of the stroke variable
 sns.countplot(x='stroke', data=df)
@@ -69,12 +55,75 @@ plt.xlabel('Stroke (0=No, 1=Yes)')
 plt.ylabel('Count')
 plt.show()
 
+# Smoking status Analysis
+print("Value of count of smoking status-\n",df['smoking_status'].value_counts())
 
-# # Plot a scatterplot of the age and bmi variables by stroke
-# sns.scatterplot(x='age', y='bmi', hue='stroke', data=df)
-# plt.title('Age and BMI by Stroke')
-# plt.xlabel('Age')
-# plt.ylabel('BMI')
-# plt.show()
+# Plot a bar chart of stroke cases by smoking status -pie
+df['smoking_status'].value_counts().plot(kind="pie")
+plt.title('Smoking Status')
+plt.ylabel('')
+plt.show()
+
+# Bar chart
+sns.countplot(x='smoking_status', hue='stroke', data=df)
+plt.title('Stroke Cases by Smoking Status')
+plt.xlabel('Smoking Status')
+plt.ylabel('Count')
+plt.show()
+
+# #Residence Type:
+print("Value of count of residence-\n",df['Residence_type'].value_counts())
+# Plot a bar chart of stroke cases by residence type
+sns.countplot(x='Residence_type', hue='stroke', data=df)
+plt.title('Stroke Cases by Residence Type')
+plt.xlabel('Residence Type')
+plt.ylabel('Count')
+plt.show()
+
+# Average Glucose Level:
+sns.regplot(x='avg_glucose_level', y='stroke', data=df, logistic=True)
+plt.title('Relationship between Average Glucose Level and Stroke')
+plt.xlabel('Average Glucose Level')
+plt.ylabel('Stroke (0=No, 1=Yes)')
+plt.show()
+
+# # Plot histograms of glucose levels for those with and without stroke
+plt.hist(df[df['stroke'] == 0]['avg_glucose_level'], alpha=0.5, label='No Stroke', bins=20)
+plt.hist(df[df['stroke'] == 1]['avg_glucose_level'], alpha=0.5, label='Stroke', bins=20)
+plt.title('Glucose Levels by Stroke Status')
+plt.xlabel('Average Glucose Level')
+plt.ylabel('Frequency')
+plt.legend()
+plt.show()
+
+# Plot histogram of glucose levels for entire dataset
+sns.histplot(data=df['avg_glucose_level'])
+plt.title('Glucose Level Distribution')
+plt.xlabel('Average Glucose Level')
+plt.ylabel('Frequency')
+plt.show()
+
+# BMI
+sns.regplot(x='bmi', y='stroke', data=df, logistic=True)
+plt.title('Relationship between BMI and Stroke')
+plt.xlabel('BMI')
+plt.ylabel('Stroke (0=No, 1=Yes)')
+plt.show()
+
+# Plot histograms of BMI for those with and without stroke
+plt.hist(df[df['stroke'] == 0]['bmi'], alpha=0.5, label='No Stroke', bins=20)
+plt.hist(df[df['stroke'] == 1]['bmi'], alpha=0.5, label='Stroke', bins=20)
+plt.title('BMI by Stroke Status')
+plt.xlabel('BMI')
+plt.ylabel('Frequency')
+plt.legend()
+plt.show()
+
+# Plot  BMI for entire dataset
+sns.histplot(data=df['bmi'])
+plt.title('BMI Distribution')
+plt.xlabel('BMI')
+plt.ylabel('Frequency')
+plt.show()
 
 
