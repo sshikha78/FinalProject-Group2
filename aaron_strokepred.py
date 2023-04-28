2b2500616eb098a57fd1599a3e19424c9fb34558
Aaron-Rock <rockam@gwu.com>
2023-04-27 20:10:33 -0400

    adding better parameters and more GridSearch options

--- aaron_strokepred.py
+++ aaron_strokepred.py
@@ -135,14 +135,12 @@ print(stroke.head(5))
 stroke['bmi'].fillna(value=stroke['bmi'].mean(),inplace=True)
 # Perform label encoding for categorical variables
 le = LabelEncoder()
-# stroke['gender'] = le.fit_transform(stroke['gender'])
-# stroke['ever_married'] = le.fit_transform(stroke['ever_married'])
-# stroke['work_type'] = le.fit_transform(stroke['work_type'])
-# stroke['Residence_type'] = le.fit_transform(stroke['Residence_type'])
-# stroke['smoking_status'] = le.fit_transform(stroke['smoking_status'])
-stroke = pd.get_dummies(stroke, columns=['gender', 'ever_married', 'work_type', 'Residence_type', 'smoking_status'])
+stroke['gender'] = le.fit_transform(stroke['gender'])
+stroke['ever_married'] = le.fit_transform(stroke['ever_married'])
+stroke['work_type'] = le.fit_transform(stroke['work_type'])
+stroke['Residence_type'] = le.fit_transform(stroke['Residence_type'])
+stroke['smoking_status'] = le.fit_transform(stroke['smoking_status'])
 X = stroke.drop('stroke', axis=1)
-X = stroke.drop('bmi', axis=1)
 y = stroke['stroke']
 y = le.fit_transform(y)
 #%%
@@ -157,54 +155,41 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 
 # Perform feature scaling using StandardScaler
 scaler = StandardScaler()
+scaler.fit(X_train)
 X_train_scaled = scaler.fit_transform(X_train)
 X_test_scaled = scaler.transform(X_test)
-# #%%
-# mlp_gs = MLPClassifier(max_iter=200)
-# parameter_space = {
-#     'hidden_layer_sizes': [(10,30,10),(20,),(10,10),(5,)],
-#     'activation': ['tanh', 'relu', 'logistic'],
-#     'solver': ['sgd', 'adam', 'lbfgs'],
-#     'alpha': [0.0001, 0.05, 0.01, 0.001],
-#     'learning_rate': ['constant','adaptive'],
-# }
-# from sklearn.model_selection import GridSearchCV
-# clf = GridSearchCV(mlp_gs, parameter_space, n_jobs=-1, cv=5)
-# clf.fit(X_train_scaled, y_train) # X is train samples and y is the corresponding labels
-#%%
-print('Best parameters found:\n', clf.best_params_)
+
 # %%
 # Neural Network
 from sklearn.neural_network import MLPClassifier
 # Neural network model
-# mlp = MLPClassifier(hidden_layer_sizes=(50,50), max_iter=1000)
-# mlp = MLPClassifier(hidden_layer_sizes=(20,), activation='tanh',learning_rate='adaptive',max_iter=100)
-mlp = MLPClassifier(max_iter=100)
+mlp = MLPClassifier(max_iter=200)
 parameter_space = {
-    'hidden_layer_sizes': [(2,2),(10,),(20,),(10,10),(5,5)],
-    'activation': ['tanh', 'relu', 'logistic'],
+    'hidden_layer_sizes': [(20,5),(5,),(10,),(50,5),(30,17),(30,15),(30,10), (35,10),(20,20)],
+    'activation': ['logistic', 'tanh', 'relu'],
     'solver': ['sgd', 'adam', 'lbfgs'],
-    'alpha': [0.0001, 0.05, 0.01, 0.001, 0.000001, 0.5],
-    'learning_rate': ['constant','adaptive'],
+    'alpha': [0.9, 0.99, 0.1, 0.0001, 0.05, 0.5],
+    'learning_rate': ['constant','adaptive', 'learning'],
 }
 from sklearn.model_selection import GridSearchCV
 clf = GridSearchCV(mlp, parameter_space, n_jobs=-1, cv=5)
-clf.fit(X_train_scaled, y_train) # X is train samples and y is the corresponding labels
+clf.fit(X_train_scaled, y_train)
+train_acc = clf.score(X_train_scaled, y_train)
+test_acc = clf.score(X_test_scaled, y_test)
+
+print(f'Train accuracy: {train_acc:.3f}')
+print(f'Test accuracy: {test_acc:.3f}')
 print('Best parameters found:\n', clf.best_params_)
-# mlp.fit(X_train_scaled, y_train)
-# plt.plot(mlp.loss_curve_)
-# plt.plot(clf.loss_curve_)
-# plt.show()
 # %%
 # y_pred = mlp.predict(X_test_scaled)
 y_pred = clf.predict(X_test_scaled)
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
 # Model evaluation
-print('Accuracy:', accuracy_score(y_test, y_pred))
-print('Precision:', precision_score(y_test, y_pred))
-print('Recall:', recall_score(y_test, y_pred))
-print('F1-score:', f1_score(y_test, y_pred))
+print(f'Accuracy: {accuracy_score(y_test, y_pred):.3f}')
+print(f'Precision: {precision_score(y_test, y_pred):.3f}')
+print(f'Recall: {recall_score(y_test, y_pred):.3f}')
+print(f'F1-score: {f1_score(y_test, y_pred):.3f}')
 #%%
 from sklearn.metrics import roc_curve, auc
 import matplotlib.pyplot as plt
@@ -225,4 +210,63 @@ plt.title('Receiver Operating Characteristic')
 plt.legend(loc="lower right")
 plt.show()
 
+# %%
+# Keras Model
+from keras.models import Sequential
+from keras.layers import Dense, Dropout
+from keras.callbacks import EarlyStopping
+from keras import regularizers
+
+# Define the model architecture
+model = Sequential()
+model.add(Dense(30, input_dim=X_train_scaled.shape[1], activation='relu', kernel_regularizer=regularizers.l2(0.0)))
+model.add(Dropout(0.0))
+model.add(Dense(1, activation='sigmoid'))
+
+# Compile the model
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+# Train the model
+history = model.fit(X_train_scaled, y_train, validation_data=(X_test_scaled, y_test), epochs=100)
+
+# Evaluate the model
+loss, accuracy = model.evaluate(X_test_scaled, y_test)
+print(f'Test loss: {loss:.3f}')
+print(f'Test accuracy: {accuracy:.3f}')
+
+# Plot the accuracy and loss curves for each epoch
+plt.plot(history.history['accuracy'], label='training accuracy')
+plt.plot(history.history['val_accuracy'], label='validation accuracy')
+plt.plot(history.history['loss'], label='training loss')
+plt.plot(history.history['val_loss'], label='validation loss')
+plt.title('Training and validation accuracy and loss')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy/Loss')
+plt.legend()
+plt.show()
+
+# %%
+# Predict probabilities for test set
+from sklearn.metrics import auc
+y_pred_keras = model.predict(X_test_scaled)
+fpr_keras, tpr_keras, thresholds_keras = roc_curve(y_test, y_pred_keras)
+auc_keras = auc(fpr_keras, tpr_keras)
+# Calculate ROC curve and AUC score
+fpr, tpr, thresholds = roc_curve(y_test, y_pred_keras)
+roc_auc = auc(fpr, tpr)
+
+# Plot ROC curve
+plt.plot(fpr, tpr, color='darkorange', lw=2, label=f'AUC = {roc_auc:.2f}')
+plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('ROC Curve')
+plt.legend(loc='lower right')
+plt.show()
+#%%
+# Model evaluation
+y_pred_keras_binary = (y_pred_keras > 0.5).astype(int)
+print(f'Accuracy: {accuracy_score(y_test, y_pred_keras_binary):.3f}')
+print(f'Precision: {precision_score(y_test, y_pred_keras_binary):.3f}')
+print(f'Recall: {recall_score(y_test, y_pred_keras_binary):.3f}')
+print(f'F1-score: {f1_score(y_test, y_pred_keras_binary):.3f}')
 # %%
